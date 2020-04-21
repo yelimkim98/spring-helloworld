7f58afd3fabf935b736a875458db9b691992dad0
gracefulBrown <sunghyun.ryu@woowahan.com>
2020-04-22 08:51:20 +0900

    feat(core): sample

--- src/main/java/wooteco/helloworld/core/UserController.java
+++ src/main/java/wooteco/helloworld/core/UserController.java
@@ -5,8 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 public class UserController {
     private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/users")
     public String retrieveUsers() {
